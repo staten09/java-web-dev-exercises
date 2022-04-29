c6a7939f1f8e84b2c027d4be001a93b9beb147a8
staten09 <staten09@gmail.com>
2022-04-28 19:56:56 -0500

    completed chapter 3 exercises

--- src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
+++ src/org/launchcode/java/demos/lsn5unittesting/test/CarTest.java
@@ -1,9 +1,24 @@
 package org.launchcode.java.demos.lsn5unittesting.test;
+//import org.junit.Test;
+//import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.launchcode.java.demos.lsn5unittesting.main.Car;
 
 public class CarTest {
 
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+    @Test
+    public void emptyTest(){
+        assertEquals(10,10,.001);
+    }
     //TODO: constructor sets gasTankLevel properly
+    @Test
+    public void testInitialGasTank(){
+        Car test_car = new Car("Toyota","Prius",10,50);
+        assertEquals(10,test_car.getGasTankLevel(),.001);
+    }
     //TODO: gasTankLevel is accurate after driving within tank range
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception
